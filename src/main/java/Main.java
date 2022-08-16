e7629956512cbba2f6841f0713b04dd003e14488
maria-grig <masha.grigorenko.89@gmail.com>
2022-08-16 15:42:28 +0300

    added post class & main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+public static void main (String[] args) {
+    Post post = new Post();
+    post.name = "Иван";
+    post.passport = "серия 1212 № 121212";
+    post.patronymic = "Иванович";
+    post.phone = "+79117267828";
+    post.surname = "Иванов";
+    post.subscription = true;
+}
+}
+
